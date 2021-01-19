bd7886c96d625f3d0925412a93e982dcc82dc282
Hyago <hyagomv@gmail.com>
2021-01-19 18:24:06 -0300

    Client/Servidor

--- server.js
+++ server.js
@@ -1,9 +1,53 @@
 // Importand o módulo NET
 const net = require('net');
 
-const socket = net.createConnection({
-  port: 8888
+
+// Cria um servido TCP ou IPC
+const server = net.createServer();
+
+// Async
+// Emite -> Event: 'listening'
+server.listen({
+  port: process.env.PORT || 9999,
+  backlog: 1
 });
+
+
+
+// --------------------------------------------------------
+// Classe: net.Server
+// --------------------------------------------------------
+
+// Event: 'close'
+server.on('close', () => {
+  console.log('[net.Server] Serivor fechado')
+});
+
+// Event: 'connection'
+server.on('connection', (clientSocket) => {
+  console.log('[net.Server] Cliente conectado')
+
+  console.log(clientSocket.bytesWritten)
+});
+
+// Event: 'error'
+server.on('error', (err) => {
+  console.log('[net.Server] Um erro ocorreu')
+  console.log(`\t[MESSAGE] ${err.message}`);
+});
+
+// Event: 'listening'
+server.on('listening', () => {
+  const { address, port } = server.address();
+
+  console.log('[net.Server] Servidor está ouvindo...')
+  console.log(`\t IP ${address}`)
+  console.log(`\t PORT ${port}`)
+})
+
+
+
+
 
 
 // --------------------------------------------------------
@@ -11,7 +55,7 @@ const socket = net.createConnection({
 // --------------------------------------------------------
 
 // Event: 'close'
-socket.on('close', (hadError) => {
+server.on('close', (hadError) => {
   if (hadError)
     console.log('[net.Socket] Socket fechado com erro');
   else
@@ -19,50 +63,50 @@ socket.on('close', (hadError) => {
 });
 
 // Event: 'connect'
-socket.on('connect', () => {
+server.on('connect', () => {
   console.log('[net.Socket] Conexão com socket estabelecida');
 });
 
 // Event: 'data'
-socket.on('data', (data) => {
+server.on('data', (data) => {
   console.log('[net.Socket] Dados recebidos');
 });
 
 // Event: 'drain'
-socket.on('drain', () => {
+server.on('drain', () => {
   console.log('[net.Socket] O buffer de gravação está vazio');
 });
 
 // Event: 'end'
-socket.on('end', () => {
+server.on('end', () => {
   console.log('[net.Socket] Cliente desconectado')
 });
 
 // Event: 'error'
-socket.on('error', (err) => {
+server.on('error', (err) => {
   console.log('[net.Socket] Ocorreu um erro:')
   console.log(`\t[MESSAGE] ${err.message}`);
 
   switch (err.code) {
     case 'EADDRINUSE': {
       console.log('[net.Socket] O sistema operacional atribuirá uma porta não utilizada arbitrária');
-      socket.listen(0);
+      server.listen(0);
       break;
     }
   }
 });
 
 // Event: 'lookup'
-socket.on('lookup', () => {
+server.on('lookup', () => {
   console.log('[net.Socket] Nome do host resolvido')
 });
 
 // Event: 'ready'
-socket.on('ready', () => {
+server.on('ready', () => {
   console.log('[net.Socket] O socket está pronto para ser usado')
 });
 
 // Event: 'timeout'
-socket.on('timeout', () => {
+server.on('timeout', () => {
   console.log('[net.Socket] O socket expirou devido a inatividade')
 });
