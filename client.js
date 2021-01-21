a5c5970cb97c22495cf37c006893ed8ed0b00e12
Hyago <hyagomv@gmail.com>
2021-01-21 18:06:10 -0300

    Adionado log de  IP e Port para Server/Client

--- client.js
+++ client.js
@@ -1,50 +1,62 @@
 // Importand o módulo NET
 const net = require('net');
 
-const client = net.createConnection({
+
+// createConnection
+const cliSocket = net.createConnection({
   port: 8888
 });
 
-client.setTimeout(5000); // 1s
+
+const time = 10000;
+console.log('[net.Socket] [Client] Timeout será definido para', time);
+cliSocket.setTimeout(time)
+
 
 /// --------------------------------------------------------
 // Classe: net.Socket
 // --------------------------------------------------------
 
 // Event: 'close'
-client.on('close', (hadError) => {
+cliSocket.on('close', (hadError) => {
   const append = hadError ? 'com erro' : 'sem erro';
   console.log('[net.Socket] [EVENT] [close] Socket fechado', append);
 });
 
+// Class: net.Socket
 // Event: 'connect'
-client.on('connect', () => {
+cliSocket.on('connect', () => {
   console.log('[net.Socket] [EVENT] [connect] Conexão com socket estabelecida');
+
+  console.log('\tServer IP Address', cliSocket.remoteAddress)
+  console.log('\tServer TCP Port', cliSocket.remotePort)
 });
 
 // Event: 'data'
-client.on('data', (data) => {
+cliSocket.on('data', (data) => {
   console.log('[net.Socket] [EVENT] [data] Dados recebidos');
+
 });
 
 // Event: 'drain'
-client.on('drain', () => {
+cliSocket.on('drain', () => {
   console.log('[net.Socket] [EVENT] [drain] O buffer de gravação está vazio');
 });
 
 // Event: 'end'
-client.on('end', () => {
+cliSocket.on('end', () => {
   console.log('[net.Socket] [EVENT] [end] Cliente desconectado')
 });
 
 // Event: 'error'
-client.on('error', (err) => {
+// Error: 'err'
+cliSocket.on('error', (err) => {
   console.log(`[net.Socket] [EVENT] [error] Ocorreu um erro:`);
 
   switch (err.code) {
     case 'EADDRINUSE': {
       console.log('\t[net.Socket] [ERROR] [Server] Uma porta será atribuida arbitráriamente');
-      client.listen(0);
+      cliSocket.listen(0);
       break;
     }
     case 'ECONNREFUSED': {
@@ -59,16 +71,16 @@ client.on('error', (err) => {
 });
 
 // Event: 'lookup'
-client.on('lookup', () => {
+cliSocket.on('lookup', () => {
   console.log('[net.Socket] [EVENT] [lookup] Nome do host resolvido')
 });
 
 // Event: 'ready'
-client.on('ready', () => {
+cliSocket.on('ready', () => {
   console.log('[net.Socket] [EVENT] [ready] O socket está pronto para ser usado')
 });
 
 // Event: 'timeout'
-client.on('timeout', () => {
+cliSocket.on('timeout', () => {
   console.log('[net.Socket] [EVENT] [timeout] O socket expirou devido a inatividade')
 });
--- server.js
+++ server.js
@@ -6,58 +6,67 @@ const net = require('net');
 // --------------------------------------------------------
 
 // Cria um servido TCP ou IPC
-//const server = net.createServer();
-const server = new net.Server();
+//const serSocket = net.createServer();
+const serSocket = new net.Server();
 
 // Async
-server.listen({
+// IP 0.0.0.0 ouvirá em todos os IPs
+serSocket.listen({
+  //host: "172.25.160.1",
   port: process.env.PORT || 9999,
   backlog: 1
 });
 
 // O servidor tem 5s para aceitar conexões
 setTimeout(() => {
-  server.close();
+  // serSocket.close();
+  //console.log('TIMEOUT............')
 }, 5000);
 
 
 // Event: 'close'
-server.on('close', () => {
+serSocket.on('close', () => {
   console.log('[net.Server] [EVENT] Serivor fechado')
 });
 
 // Event: 'connection'
-server.on('connection', (client) => {
+// <net.Socket> cliSocket
+serSocket.on('connection', (cliSocket) => {
   console.log('[net.Server] [EVENT] [connection] Cliente conectado')
 
-  client.setTimeout(5000)
+  console.log('\tClient IP Address', cliSocket.remoteAddress)
+  console.log('\tClient TCP Port', cliSocket.remotePort)
+
+  const time = 10000;
+  console.log('[net.Server] [Client] Timeout será definido para', time);
+  cliSocket.setTimeout(time)
 
   // Event: 'error'
-  client.on('error', (err) => {
+  cliSocket.on('error', (err) => {
     switch (err.code) {
       case 'ECONNRESET': {
-        console.log('\t[net.Socket] [ERROR] [Client] O cliente fechou a conexão abruptamente ');
+        console.log('\t[net.Socket] [ERROR] [Client] O cliSockete fechou a conexão abruptamente ');
         break;
       }
     }
   })
 
   // Event: 'timeout'
-  client.on('timeout', () => {
+  cliSocket.on('timeout', () => {
     console.log('[net.Socket] [EVENT] [timeout] O socket expirou devido a inatividade')
-    client.end()
+    cliSocket.end()
   });
 });
 
 // Event: 'error'
-server.on('error', (err) => {
+serSocket.on('error', (err) => {
   console.log('[net.Server] [EVENT] [error] Um erro ocorreu')
   console.log(`\t[MESSAGE] ${err.message}`);
 });
 
 // Event: 'listening'
-server.on('listening', () => {
-  const { address, port } = server.address();
+serSocket.on('listening', () => {
+  const { address, port } = serSocket.address();
 
   console.log('[net.Server] [EVENT] [listening] Servidor está ouvindo...')
   console.log(`\t IP ${address}`)
@@ -70,55 +79,59 @@ server.on('listening', () => {
 // --------------------------------------------------------
 
 // Event: 'close'
-server.on('close', (hadError) => {
+serSocket.on('close', (hadError) => {
   const append = hadError ? 'com erro' : 'sem erro';
   console.log('[net.Socket] [close] Socket fechado', append);
 });
 
 // Event: 'connect'
-server.on('connect', () => {
+serSocket.on('connect', () => {
   console.log('[net.Socket] [connect] Conexão com socket estabelecida');
 });
 
 // Event: 'data'
-server.on('data', (data) => {
+serSocket.on('data', (data) => {
   console.log('[net.Socket] [data] Dados recebidos');
 });
 
 // Event: 'drain'
-server.on('drain', () => {
+serSocket.on('drain', () => {
   console.log('[net.Socket] [drain] O buffer de gravação está vazio');
 });
 
 // Event: 'end'
-server.on('end', () => {
+serSocket.on('end', () => {
   console.log('[net.Socket] [end] Cliente desconectado')
 });
 
 // Event: 'error'
-server.on('error', (err) => {
-  console.log(`[net.Socket] [error] Ocorreu um erro: ${err.message}`);
+serSocket.on('error', (err) => {
+  console.log(`[net.Socket] [error] Ocorreu um erro:`);
 
   switch (err.code) {
     case 'EADDRINUSE': {
       console.log('[net.Socket] [EADDRINUSE] Uma porta será atribuida arbitráriament');
-      server.listen(0);
+      serSocket.listen(0);
+      break;
+    }
+    case 'EADDRNOTAVAIL': {
+      console.log('[net.Socket] [EADDRINUSE] O endereço não está disponível');
       break;
     }
   }
 });
 
 // Event: 'lookup'
-server.on('lookup', () => {
+serSocket.on('lookup', () => {
   console.log('[net.Socket] [lookup] Nome do host resolvido')
 });
 
 // Event: 'ready'
-server.on('ready', () => {
+serSocket.on('ready', () => {
   console.log('[net.Socket] [ready] O socket está pronto para ser usado')
 });
 
 // Event: 'timeout'
-server.on('timeout', () => {
+serSocket.on('timeout', () => {
   console.log('[net.Socket] [timeout] O socket expirou devido a inatividade')
 });
