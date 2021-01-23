5cc67a236295c9cbc633e715ad145e0e9e015848
Hyago <hyagomv@gmail.com>
2021-01-23 00:08:31 -0300

    Várias melhorias

--- client.js
+++ client.js
@@ -1,103 +1,229 @@
-// Importand o módulo NET
+// Importa o  módulo NET
 import net from 'net';
 
-
-// createConnection
-const cliSocket = net.createConnection({
-  port: 444
-});
-
-
-const time = 50000;
-console.log('[net.Socket] [Client] Timeout será definido para', time);
-cliSocket.setTimeout(time)
-
-
-/// --------------------------------------------------------
-// Classe: net.Socket
+// ----------------------------------------------------------------
+//                        Client Socket
+//                        Class: net.Socket
 // --------------------------------------------------------
 
-// Event: 'close'
-cliSocket.on('close', (hadError) => {
+const socket = new net.Socket({
+  allowHalfOpen: false,  // Padrão
+});
+
+// Async
+// Doc: Inicie uma conexão em um determinado soquete.
+// Nota: O evento 'connect' é emitido quando um conexão é estabelecida
+// Nota: O evento 'error' é emitido quando ocorrer um error
+// Nota: Só deve ser usada para reconectar um socket depois do evento 'close' ter sido emitido
+socket.connect({
+  port: 8888,
+  host: '::1',
+  localAddress: '::1',
+  localPort: 7777
+});
+
+// ----------------------------------------------------------------
+
+// Doc: Pausa a leitura dos dados. 
+// Nota: O evento 'data' não será emitido
+setTimeout(() => {
+  socket.pause();
+}, 1000);
+
+// ----------------------------------------------------------------
+
+// Doc: Retoma a leitura dos dados. 
+// Nota: Usado após a chamda de socket.pause()
+setTimeout(() => {
+  socket.resume();
+}, 2000);
+
+// ----------------------------------------------------------------
+
+socket.setNoDelay(true);
+
+// ----------------------------------------------------------------
+
+// Nota: Emite o evento 'timeout'
+// Nota: Deve-se chamar manualmente socket.end()ou socket.destroy() encerrar a conexão.
+socket.setTimeout(10000);
+
+// ----------------------------------------------------------------
+//                        Client Socket
+//                        Class: net.Socket
+//                        Event: 'close'
+// ----------------------------------------------------------------
+
+// boolean: hadError: TRUE se foi fechado devido a um erro de transmissão.
+// Doc: Emitido quando o socket está totalmente fechado
+socket.on('close', (hadError) => {
   const append = hadError ? 'com erro' : 'sem erro';
-  console.log('[net.Socket] [EVENT] [close] Socket fechado', append);
+  console.log(
+    '[net.Socket] [Event] [close] O soquete do cliente foi fechado', append, 'de transmissão'
+  );
 });
 
-// Class: net.Socket
-// Event: 'connect'
-cliSocket.on('connect', () => {
-  console.log('[net.Socket] [EVENT] [connect] Conexão estabelecida');
 
-  console.log('\tServer IP Address', cliSocket.remoteAddress)
-  console.log('\tServer TCP Port', cliSocket.remotePort)
+// ----------------------------------------------------------------
+//                        Client Socket
+//                        Class: net.Socket
+//                        Event: 'connect'
+// ----------------------------------------------------------------
+
+// Doc: Emitido quando uma conexão de soquete é estabelecida com sucesso
+socket.on('connect', () => {
+  console.log(
+    '[net.Socket] [Event] [connect] Conexão entre Cliente/Servidor estabelecida'
+  );
+
+  console.log('\t', socket.address());
+  console.log('\tEndereço IP remoto', socket.remoteAddress);
+  console.log('\tFamília de IPs remotos', socket.remoteFamily);
+  console.log('\tNúmero da porta remota', socket.remotePort);
 });
 
-// Event: 'data'
-cliSocket.on('data', (data) => {
-  console.log('[net.Socket] [EVENT] [data] Dados recebidos');
 
+// ----------------------------------------------------------------
+//                        Client Socket
+//                        Class: net.Socket
+//                        Event: 'data'
+// ----------------------------------------------------------------
+
+// Buffer | String: data
+// Doc: Emitido quando os dados são recebidos.
+socket.on('data', (data) => {
+  console.log(
+    '[net.Socket] [Event] [data] O cliente enviou dados...'
+  );
+  // A quantidade de bytes recebidos.
+  console.log('\tQuantidade de bytes:', socket.bytesRead);
+  console.log('\tDado Recebido:', data.toString());
 });
 
-// Event: 'drain'
-cliSocket.on('drain', () => {
-  console.log('[net.Socket] [EVENT] [drain] O buffer de gravação está vazio');
+// ----------------------------------------------------------------
+//                        Client Socket
+//                        Class: net.Socket
+//                        Event: 'drain'
+// ----------------------------------------------------------------
+
+// Doc: Emitido quando o buffer de gravação fica vazio.
+// Nota: Pode ser usado para acelerar uploads.
+socket.on('drain', () => {
+  console.log(
+    '[net.Socket] [Event] [drain] O buffer de gravação está vazio'
+  );
 });
 
-// Event: 'end'
-cliSocket.on('end', () => {
-  console.log('[net.Socket] [EVENT] [end] Fim conexão')
+/// ----------------------------------------------------------------
+//                        Client Socket
+//                        Class: net.Socket
+//                        Event: 'end'
+// ----------------------------------------------------------------
+
+// Doc: Emitido quando a outra extremidade do socket envia um pacote FIN
+socket.on('end', () => {
+  console.log(
+    '[net.Socket] [Event] [end] O socket da outra extremidade enviou um pacote FIN'
+  );
 });
 
-// Event: 'error'
-// Error: 'err'
-cliSocket.on('error', (err) => {
-  console.log(`[net.Socket] [EVENT] [error] Ocorreu um erro:`);
+// ----------------------------------------------------------------
+//                        Client Socket
+//                        Class: net.Socket
+//                        Event: 'error'
+// ----------------------------------------------------------------
 
+// Error: err
+// Doc: Emitido quando ocorre um erro.
+// Nota: O evento 'close' será emitido após este evento.
+socket.on('error', (err) => {
+  let append;
   switch (err.code) {
+    case 'EADDRNOTAVAIL': {
+      append = `Endereço não disponível: ${err.address}`;
+      break;
+    }
     case 'EADDRINUSE': {
-      console.log('\t[net.Socket] [ERROR] [Server] Uma porta será atribuida arbitráriamente');
-      cliSocket.listen(0);
-      break;
-    }
-    case 'ECONNREFUSED': {
-      console.log('\t[net.Socket] [ERROR] [Server] Conexão recusada pelo servidor')
-      break;
-    }
-    case 'ECONNRESET': {
-      console.log('\t[net.Socket] [ERROR] [Server] O servidor fechou a conexão abruptamente ');
-      break;
-    }
-    case 'ERR_STREAM_WRITE_AFTER_END': {
-
+      append = `Endereço já em uso ${err.address}`;
       break;
     }
     default: {
-      console.log(err.code);
+      append = err.code;
+      break;
+    }
+  }
+  console.log(
+    '[net.Socket] [Event] [error]', append
+  );
+})
+
+// ----------------------------------------------------------------
+//                        Client Socket
+//                        Class: net.Socket
+//                        Event: 'lookup'
+// ----------------------------------------------------------------
+
+// Error | nulll: err
+// string: address
+// string | null: family
+// string: host
+// Doc: Emitido após resolver o nome do host, mas antes de conectar.
+// Nota: Não aplicável a soquetes Unix.
+socket.on('lookup', (err, address, family, host) => {
+  console.log(
+    '[net.Socket] [Event] [lookup] Nome do host resolvido'
+  );
+});
+
+// ----------------------------------------------------------------
+//                        Client Socket
+//                        Class: net.Socket
+//                        Event: 'ready'
+// ----------------------------------------------------------------
+
+// Doc: Emitido quando um soquete está pronto para ser usado.
+// Nota: Emitido após o evento 'connect'
+socket.on('ready', () => {
+  console.log(
+    '[net.Socket] [Event] [ready] O socket está pronto para ser usado'
+  );
+
+  switch (socket.readyState) {
+    case 'open':
+    case 'writeOnly': {
+      // Doc: Envia dados pelo soquete.
+      const sucess = socket.write('Testando 123... testando...', 'utf8', () => {
+        console.log(
+          '\t[Info] Todos os dados foram gravados'
+        );
+      });
+      if (sucess)
+        console.log(
+          '\t[Info] Todos os dados foram liberados com sucesso para o buffer do kernel'
+        );
+      else
+        // O evento drain' será emitido quando o buffer estiver novamente livre.
+        console.log(
+          '\t[Info] Todos ou parte dos dados foram enfileirados na memória do usuário'
+        );
       break;
     }
   }
 });
 
-// Event: 'lookup'
-cliSocket.on('lookup', () => {
-  console.log('[net.Socket] [EVENT] [lookup] Nome do host resolvido')
-});
+// ----------------------------------------------------------------
+//                        Client Socket
+//                        Class: net.Socket
+//                        Event: 'timeout'
+// ----------------------------------------------------------------
 
-// Event: 'ready'
-cliSocket.on('ready', () => {
-  console.log('[net.Socket] [EVENT] [ready] O socket está pronto para ser usado')
-
-  setInterval(() => {
-    cliSocket.write("A")
-  }, 2000);
-
-  setTimeout(() => {
-    cliSocket.end();
-  }, 5000);
-
-});
-
-// Event: 'timeout'
-cliSocket.on('timeout', () => {
-  console.log('[net.Socket] [EVENT] [timeout] O socket expirou devido a inatividade')
+// Doc: Emitido se o soquete expirar devido à inatividade
+// Nota: Deve-se fechar manualmente a conexão.
+socket.on('timeout', () => {
+  console.log(
+    '[net.Socket] [Event] [timeout] O socket expirou devido a inatividade'
+  );
+  // Eencerrar a conexão.
+  socket.end();
+  // socket.destroy()
 });
--- server.js
+++ server.js
@@ -1,110 +1,152 @@
 // Importa o  módulo NET
 import net from 'net';
+import { setTimeout } from 'timers';
 
 
 // --------------------------------------------------------
-// Class: net.Server
+//                        Server TCP
+//                        Class: net.Server
 // --------------------------------------------------------
 
-// Cria um servido TCP ou IPC
-//const serSocket = net.createServer();
-const serSocket = new net.Server();
-
-// Async
-// IP 0.0.0.0 ouvirá em todos os IPs
-serSocket.listen({
-  //host: "172.25.160.1",
-  //port: process.env.PORT || 9999,
-  port: 444,
-  backlog: 1
+// Cria um servido TCP
+const server = new net.Server({
+  allowHalfOpen: false,  // Padrão
+  pauseOnConnect: false  // Padrão
 });
 
-// O servidor tem 5s para aceitar conexões
+// ----------------------------------------------------------------
+
+// Async
+// Doc: Impede o servidor de aceitar novas conexões e mantém as conexões existentes.
+// Nota: O servidor só é fechado quando todas as conexões são encerradas
+// Nota: Emite o evento 'close'
 setTimeout(() => {
-  // serSocket.close();
-  //console.log('TIMEOUT............')
+  server.close();
 }, 5000);
 
 // ----------------------------------------------------------------
 
-// Class: net.Server
-// Event: 'close'
-// Doc: Este evento não será emitido até que todas as conexões sejam encerradas.
-serSocket.on('close', () => {
-  console.log(
-    '[net.Server] [Event] [close] Todas as conexões dos Clientes com o Servidor foram encerradas'
-  );
-});
+// Async
+// Doc: Obtem o número de conexões simultâneas no servidor
+// Nota: Emite o evento 'listening'
+setInterval(() => {
+  server.getConnections((err, count) => {
+    console.log(count)
+  });
+}, 5000);
 
 // ----------------------------------------------------------------
 
-// Class: net.Server
-// Event: 'connection'
-// net.Socket: clientSocket: Conexão com um Cliente
-// Doc: clientSocket é criado por Node.js
-serSocket.on('connection', (clientSocket) => {
+// Async
+// Doc: O servidor começará a ouvir por conexões
+// Nota: Se port for omitido ou for 0, o sistema operacional atribuirá uma porta não utilizada arbitrária
+server.listen({
+  port: process.env.PORT || 9999,
+  host: '::1', // 0:0:0:0:0:0:0:1 Local addresses
+  ipv6Only: true
+});
+
+// Doc: Rejeitar novas conexões quando o valor máximo de conexões for atingida
+server.maxConnections = 2
+
+// ----------------------------------------------------------------
+//                        Server TCP
+//                        Class: net.Server
+//                        Event: 'close'
+// ----------------------------------------------------------------
+
+// Doc: Emitido quando o servidor fecha
+// Nota: Este evento não será emitido até que todas as conexões sejam encerradas.
+server.on('close', (e) => {
+  console.log(
+    '[net.Server] [Event] [close] Todas as conexões Cliente/Servidor foram encerradas'
+  );
+  process.exit();
+});
+
+// ----------------------------------------------------------------
+//                        Server TCP
+//                        Class: net.Server
+//                        Event: 'connection'
+// ----------------------------------------------------------------
+
+// net.Socket: clientSocket
+// Doc: Emitido quando uma nova conexão é feita
+// Nota: clientSocket é criado por Node.js
+server.on('connection', (clientSocket) => {
   console.log(
     '[net.Server] [Event] [connection] Conexão entre Cliente/Servidor estabelecida'
   );
 
   // ----------------------------------------------------------------
   //                        Client Socket
+  //                        Class: net.Socket
+  //                        Event: 'close'
   // ----------------------------------------------------------------
 
-  // Class: net.Socket
-  // Event: 'close'
   // boolean: hadError: TRUE se foi fechado devido a um erro de transmissão.
   // Doc: Emitido quando o socket está totalmente fechado
   clientSocket.on('close', (hadError) => {
     const append = hadError ? 'com erro' : 'sem erro';
     console.log(
-      '[net.Socket] [event] [close] Socket fechado', append, 'de transmissão'
+      '[net.Socket] [Event] [close] O soquete do cliente foi fechado', append, 'de transmissão'
     );
   });
 
+
+  // ----------------------------------------------------------------
+  //                        Client Socket
+  //                        Class: net.Socket
+  //                        Event: 'connect'
   // ----------------------------------------------------------------
 
-  // Class: net.Socket
-  // Event: 'connect'
   // Doc: Emitido quando uma conexão de soquete é estabelecida com sucesso
   clientSocket.on('connect', () => {
     console.log(
-      '[net.Server] [Event] [connection] Conexão entre Cliente/Servidor estabelecida'
+      '[net.Socket] [Event] [connect] Conexão entre Cliente/Servidor estabelecida'
     );
 
-    console.log('\Client IP Address', clientSocket.remoteAddress)
+    console.log('\tClient IP Address', clientSocket.remoteAddress)
     console.log('\tClient TCP Port', clientSocket.remotePort)
   });
 
   // ----------------------------------------------------------------
+  //                        Client Socket
+  //                        Class: net.Socket
+  //                        Event: 'data'
+  // ----------------------------------------------------------------
 
-  // Class: net.Socket
-  // Event: 'data'
   // Buffer | String: data
   // Doc: Emitido quando os dados são recebidos.
   clientSocket.on('data', (data) => {
     console.log(
       '[net.Socket] [Event] [data] O cliente enviou dados...'
     );
+    // A quantidade de bytes recebidos.
+    console.log('\tQuantidade de bytes:', clientSocket.bytesRead);
     console.log('\tDado Recebido:', data.toString());
   });
 
   // ----------------------------------------------------------------
+  //                        Client Socket
+  //                        Class: net.Socket
+  //                        Event: 'drain'
+  // ----------------------------------------------------------------
 
-  // Class: net.Socket
-  // Event: 'drain'
   // Doc: Emitido quando o buffer de gravação fica vazio.
   // Nota: Pode ser usado para acelerar uploads.
   clientSocket.on('drain', () => {
     console.log(
-      '[net.Socket] [Evet] [drain] O buffer de gravação está vazio'
+      '[net.Socket] [Event] [drain] O buffer de gravação está vazio'
     );
   });
 
+  /// ----------------------------------------------------------------
+  //                        Client Socket
+  //                        Class: net.Socket
+  //                        Event: 'end'
   // ----------------------------------------------------------------
 
-  // Class: net.Socket
-  // Event: 'end'
   // Doc: Emitido quando a outra extremidade do socket envia um pacote FIN
   clientSocket.on('end', () => {
     console.log(
@@ -113,9 +155,11 @@ serSocket.on('connection', (clientSocket) => {
   });
 
   // ----------------------------------------------------------------
+  //                        Client Socket
+  //                        Class: net.Socket
+  //                        Event: 'error'
+  // ----------------------------------------------------------------
 
-  // Class: net.Socket
-  // Event: 'error'
   // Error: err
   // Doc: Emitido quando ocorre um erro.
   // Nota: O evento 'close' será emitido após este evento.
@@ -123,23 +167,14 @@ serSocket.on('connection', (clientSocket) => {
     console.log(
       '[net.Socket] [Event] [error] Ocorreu um erro'
     );
-    switch (err.code) {
-      case 'ECONNRESET': {
-        //console.log('\t[net.Socket] [ERROR] [Client] O clietne fechou a conexão abruptamente');
-        console.log('\t[net.Socket] [ERROR] [Client] O cliete fechou a conexão com a flag RST e não com a flag FIN');
-        break;
-      }
-    }
-
-    clientSocket.on('end', () => {
-      console.log('\t[net.Socket] [EVENT] [end] [Client] O cliete fechou a conexão com a flag FIN');
-    });
   })
 
   // ----------------------------------------------------------------
+  //                        Client Socket
+  //                        Class: net.Socket
+  //                        Event: 'lookup'
+  // ----------------------------------------------------------------
 
-  // Class: net.Socket
-  // Event: 'lookup'
   // Error | nulll: err
   // string: address
   // string | null: family
@@ -153,9 +188,11 @@ serSocket.on('connection', (clientSocket) => {
   });
 
   // ----------------------------------------------------------------
+  //                        Client Socket
+  //                        Class: net.Socket
+  //                        Event: 'ready'
+  // ----------------------------------------------------------------
 
-  // Class: net.Socket
-  // Event: 'ready'
   // Doc: Emitido quando um soquete está pronto para ser usado.
   // Nota: Emitido após o evento 'connect'
   clientSocket.on('ready', () => {
@@ -165,9 +202,11 @@ serSocket.on('connection', (clientSocket) => {
   });
 
   // ----------------------------------------------------------------
+  //                        Client Socket
+  //                        Class: net.Socket
+  //                        Event: 'timeout'
+  // ----------------------------------------------------------------
 
-  // Class: net.Socket
-  // Event: 'timeout'
   // Doc: Emitido se o soquete expirar devido à inatividade
   // Nota: Deve-se fechar manualmente a conexão.
   clientSocket.on('timeout', () => {
@@ -179,81 +218,208 @@ serSocket.on('connection', (clientSocket) => {
 });
 
 // ----------------------------------------------------------------
+//                        Server TCP
+//                        Class: net.Server
+//                        Event: 'error'
+// ----------------------------------------------------------------
 
-// Event: 'error'
-serSocket.on('error', (err) => {
-  console.log('[net.Server] [EVENT] [error] Um erro ocorreu')
-  console.log(`\t[MESSAGE] ${err.message}`);
-});
-
-// Event: 'listening'
-serSocket.on('listening', () => {
-  const { address, port } = serSocket.address();
-
-  console.log('[net.Server] [EVENT] [listening] Servidor está ouvindo...')
-  console.log(`\t IP ${address}`)
-  console.log(`\t PORT ${port}`)
-})
-
-
-// --------------------------------------------------------
-// Classe: net.Socket
-// --------------------------------------------------------
-
-// Event: 'close'
-serSocket.on('close', (hadError) => {
-  const append = hadError ? 'com erro' : 'sem erro';
-  console.log('[net.Socket] [close] Socket fechado', append);
-});
-
-// Event: 'connect'
-serSocket.on('connect', () => {
-  console.log('[net.Socket] [connect] Conexão com socket estabelecida');
-});
-
-// Event: 'data'
-serSocket.on('data', (data) => {
-  console.log('[net.Socket] [data] Dados recebidos');
-});
-
-// Event: 'drain'
-serSocket.on('drain', () => {
-  console.log('[net.Socket] [drain] O buffer de gravação está vazio');
-});
-
-// Event: 'end'
-serSocket.on('end', () => {
-  console.log('[net.Socket] [end] Cliente desconectado')
-});
-
-// Event: 'error'
-serSocket.on('error', (err) => {
-  console.log(`[net.Socket] [error] Ocorreu um erro:`);
-
+// Doc: Emitido quando ocorre um erro
+// Nota: O evento'close'  não será emitido automaticamente
+server.on('error', (err) => {
+  let append;
   switch (err.code) {
-    case 'EADDRINUSE': {
-      console.log('[net.Socket] [EADDRINUSE] Uma porta será atribuida arbitráriament');
-      serSocket.listen(0);
+    case 'EADDRNOTAVAIL': {
+      append = `Endereço não disponível: ${err.address}`;
       break;
     }
-    case 'EADDRNOTAVAIL': {
-      console.log('[net.Socket] [EADDRINUSE] O endereço não está disponível');
+    case 'EADDRINUSE': {
+      append = `Endereço já em uso ${err.address}`;
+      break;
+    }
+    default: {
+      append = err.code;
       break;
     }
   }
+  console.log(
+    '[net.Socket] [Event] [error]', append
+  );
+})
+
+// ----------------------------------------------------------------
+//                        Server TCP
+//                        Class: net.Server
+//                        Event: 'listening'
+// ----------------------------------------------------------------
+
+// Doc: Emitido quando o servidor está vinculada a uma porta
+server.on('listening', () => {
+  console.log(
+    '[net.Server] [Event] [listening] '
+  );
+
+  console.log('\t', server.address());
+})
+
+// ----------------------------------------------------------------
+//                        Server Socket
+//                        Class: net.Socket
+//                        Event: 'close'
+// ----------------------------------------------------------------
+
+// boolean: hadError: TRUE se foi fechado devido a um erro de transmissão.
+// Doc: Emitido quando o socket está totalmente fechado
+server.on('close', (hadError) => {
+  const append = hadError ? 'com erro' : 'sem erro';
+  console.log(
+    '[net.Socket] [close] O soquete do servidor foi fechado', append, 'de transmissão'
+  );
 });
 
+// ----------------------------------------------------------------
+//                        Server Socket
+//                        Class: net.Socket
+//                        Event: 'connect'
+// ----------------------------------------------------------------
+
+// Class: net.Socket
+// Event: 'connect'
+// Doc: Emitido quando uma conexão de soquete é estabelecida com sucesso
+server.on('connect', () => {
+  console.log(
+    '[net.Socket] [Event] [connect] Conexão entre Cliente/Servidor estabelecida'
+  );
+
+  console.log(
+    '\tServer IP Address', server.remoteAddress
+  );
+  console.log(
+    '\ttServer TCP Port', server.remotePort
+  );
+});
+
+// ----------------------------------------------------------------
+//                        Server Socket
+//                        Class: net.Socket
+//                        Event: 'data'
+// ----------------------------------------------------------------
+
+// Class: net.Socket
+// Event: 'data'
+// Buffer | String: data
+// Doc: Emitido quando os dados são recebidos.
+server.on('data', (data) => {
+  console.log(
+    '[net.Socket] [Event] [data] O cliente enviou dados...'
+  );
+  console.log('\tDado Recebido:', data.toString());
+});
+
+// ----------------------------------------------------------------
+//                        Server Socket
+//                        Class: net.Socket
+//                        Event: 'drain'
+// ----------------------------------------------------------------
+
+// Class: net.Socket
+// Event: 'drain'
+// Doc: Emitido quando o buffer de gravação fica vazio.
+// Nota: Pode ser usado para acelerar uploads.
+server.on('drain', () => {
+  console.log(
+    '[net.Socket] [Event] [drain] O buffer de gravação está vazio'
+  );
+});
+
+// ----------------------------------------------------------------
+//                        Server Socket
+//                        Class: net.Socket
+//                        Event: 'end'
+// ----------------------------------------------------------------
+
+// Class: net.Socket
+// Event: 'end'
+// Doc: Emitido quando a outra extremidade do socket envia um pacote FIN
+server.on('end', () => {
+  console.log(
+    '[net.Socket] [Event] [end] O socket da outra extremidade enviou um pacote FIN'
+  );
+});
+
+// ----------------------------------------------------------------
+//                        Server Socket
+//                        Class: net.Socket
+//                        Event: 'error'
+// ----------------------------------------------------------------
+
+// Class: net.Socket
+// Event: 'error'
+// Error: err
+// Doc: Emitido quando ocorre um erro.
+// Nota: O evento 'close' será emitido após este evento.
+server.on('error', (err) => {
+  let append;
+  switch (err.code) {
+    case 'EADDRINUSE': {
+      append = 'Endereço já em uso';
+      break;
+    }
+  }
+
+  console.log(
+    '[net.Socket] [Event] [error]', append
+  );
+})
+
+// ----------------------------------------------------------------
+//                        Server Socket
+//                        Class: net.Socket
+//                        Event: 'lookup'
+// ----------------------------------------------------------------
+
+// Class: net.Socket
 // Event: 'lookup'
-serSocket.on('lookup', () => {
-  console.log('[net.Socket] [lookup] Nome do host resolvido')
+// Error | nulll: err
+// string: address
+// string | null: family
+// string: host
+// Doc: Emitido após resolver o nome do host, mas antes de conectar.
+// Nota: Não aplicável a soquetes Unix.
+server.on('lookup', (err, address, family, host) => {
+  console.log(
+    '[net.Socket] [Event] [lookup] Nome do host resolvido'
+  );
 });
 
+// ----------------------------------------------------------------
+//                        Server Socket
+//                        Class: net.Socket
+//                        Event: 'ready'
+// ----------------------------------------------------------------
+
+// Class: net.Socket
 // Event: 'ready'
-serSocket.on('ready', () => {
-  console.log('[net.Socket] [ready] O socket está pronto para ser usado')
+// Doc: Emitido quando um soquete está pronto para ser usado.
+// Nota: Emitido após o evento 'connect'
+server.on('ready', () => {
+  console.log(
+    '[net.Socket] [Event] [ready] O socket está pronto para ser usado'
+  );
 });
 
+// ----------------------------------------------------------------
+//                        Server Socket
+//                        Class: net.Socket
+//                        Event: 'timeout'
+// ----------------------------------------------------------------
+
+// Class: net.Socket
 // Event: 'timeout'
-serSocket.on('timeout', () => {
-  console.log('[net.Socket] [timeout] O socket expirou devido a inatividade')
+// Doc: Emitido se o soquete expirar devido à inatividade
+// Nota: Deve-se fechar manualmente a conexão.
+server.on('timeout', () => {
+  console.log(
+    '[net.Socket] [Event] [timeout] O socket expirou devido a inatividade'
+  );
+  clientSocket.end(); // ?
 });
